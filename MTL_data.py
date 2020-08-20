62fee74f354d46043358922985f701388f2231d2
Michael Pedruski <mpedruski@gmail.com>
2020-08-19 21:06:58 -0400

    Added dummy data to git

---
+++ MTL_data.py
@@ -0,0 +1,64 @@
+from MTL_Classes import Location
+
+loc0 = Location (
+    name = "Westmount Park",
+    long = -73.59717,
+    lat = 45.48146,
+    quartier = "Westmount",
+    story = "On a nice sunny day I sat here and watched the ducks",
+    year = 2014,
+    month = 6,
+)
+
+loc1 = Location (
+    name = "L'Oratoire Saint-Joseph",
+    long = -73.61749,
+    lat = 45.49234,
+    quartier = "Côte des Neiges",
+    story = "Once upon a time I went to the Oratory. It was nice.",
+    year = 2019,
+    month = 9,
+)
+
+loc2 = Location (
+    name = "Cha du Mont-Royal",
+    long = -73.58721,
+    lat = 45.50374,
+    quartier = "Ville-Marie",
+    story = "I ran to the cha in March. It was pretty cold, but sunny, so I \
+        couldn't really complain. Frankly, I had a good time.",
+    year = 2020,
+    month = 3,
+)
+
+loc3 = Location (
+    name = "Westmount Library",
+    long = -73.59920,
+    lat = 45.48185,
+    quartier = "Westmount",
+    story = "Once upon a time I went to the Westmount library and read a book",
+    year = 2016,
+    month = 12,
+)
+
+loc4 = Location (
+    name = "Westmount Library = II",
+    long = -73.59921,
+    lat = 45.48184,
+    quartier = "Westmount",
+    story = "It's Bloomsday at the Wesmount Library! Much better than skating.",
+    year = 2019,
+    month = 6,
+)
+
+loc5 = Location (
+    name = "Lac aux castors",
+    long = -73.59778,
+    lat = 45.49790,
+    quartier = "Ville-Marie",
+    story = "I've never been skating here, no matter how sunny it is.",
+    year = 2012,
+    month = 1,
+)
+
+locations = [loc0, loc1, loc2, loc3, loc4, loc5]
